b7e1a5d1498cc626d7b57c7bcf2ef66b8ebcc157
rbrands <mail@robert-brands.com>
2020-10-27 19:43:27 +0100

    Get subscription only once

--- MeetUpPlanner/Shared/NotificationSubscription.cs
+++ MeetUpPlanner/Shared/NotificationSubscription.cs
@@ -6,14 +6,11 @@ namespace MeetUpPlanner.Shared
 {
     public class NotificationSubscription : CosmosDBEntity
     {
+        public string PlannerUrl { get; set; }
         public string UserFirstName { get; set; }
-
         public string UserLastName { get; set; }
-
         public string Url { get; set; }
-
         public string P256dh { get; set; }
-
         public string Auth { get; set; }
     }
 }
