8c3a03bd50dc9ed26ca120a86c262333415ee70e
rbrands <mail@robert-brands.com>
2020-11-18 08:28:50 +0100

    Use spinner element for loading

---
+++ MeetUpPlanner/Shared/BlobAccessSignature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeetUpPlanner.Shared
+{
+    public class BlobAccessSignature
+    {
+        public Uri Sas { get; set; }
+        public string BlobName { get; set; }
+        public string FileName { get; set; }
+
+        public BlobAccessSignature()
+        {
+
+        }
+        public BlobAccessSignature(string blobName, string fileName, Uri sas)
+        {
+            BlobName = blobName;
+            FileName = fileName;
+            Sas = sas;
+        }
+    }
+}
