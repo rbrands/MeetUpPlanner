5417126addcb942db2d2004bc91c7aa78c4f5664
rbrands <mail@robert-brands.com>
2021-11-08 11:10:42 +0100

    Version

--- MeetUpPlanner/Server/Controllers/UtilController.cs
+++ MeetUpPlanner/Server/Controllers/UtilController.cs
@@ -20,7 +20,7 @@ namespace MeetUpPlanner.Server.Controllers
     {
         private readonly MeetUpFunctions _meetUpFunctions;
         private readonly ILogger<UtilController> logger;
-        const string serverVersion = "2021-10-25";
+        const string serverVersion = "2021-11-08";
         string functionsVersion = "tbd";
 
         public UtilController(ILogger<UtilController> logger, MeetUpFunctions meetUpFunctions)
