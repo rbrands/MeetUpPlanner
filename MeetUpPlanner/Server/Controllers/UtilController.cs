44e5d981c60a5ca6d53553ac4e9f1e69b3a84162
Robert Brands <mail@robert-brands.com>
2022-07-29 12:14:07 +0200

    Azure Functions migration to .NET 6 (#104)
    Co-authored-by: Robert Brands (RiwaAdmin) <rbadmin@riwa4.de>

--- MeetUpPlanner/Server/Controllers/UtilController.cs
+++ MeetUpPlanner/Server/Controllers/UtilController.cs
@@ -20,7 +20,7 @@ namespace MeetUpPlanner.Server.Controllers
     {
         private readonly MeetUpFunctions _meetUpFunctions;
         private readonly ILogger<UtilController> logger;
-        const string serverVersion = "2022-07-21";
+        const string serverVersion = "2022-07-28";
         string functionsVersion = "tbd";
 
         public UtilController(ILogger<UtilController> logger, MeetUpFunctions meetUpFunctions)
