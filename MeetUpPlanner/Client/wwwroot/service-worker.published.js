a461cd63ebda2a5bbd0027a8b6bff78ace24313a
Robert Brands (RiwaAdmin) <rbadmin@riwa4.de>
2022-08-05 13:53:37 +0200

    Offline loading disabled (commented out)

--- MeetUpPlanner/Client/wwwroot/service-worker.published.js
+++ MeetUpPlanner/Client/wwwroot/service-worker.published.js
@@ -1,13 +1,14 @@
 ﻿// Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
-self.importScripts('./service-worker-assets.js');
-self.addEventListener('install', event => {
-    self.skipWaiting();
-    event.waitUntil(onInstall(event))
-});
-self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
-self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+// self.importScripts('./service-worker-assets.js');
+// self.addEventListener('install', event => {
+//     self.skipWaiting();
+//     event.waitUntil(onInstall(event))
+//});
+// self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
+// self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+self.addEventListener('fetch', () => { });
 self.addEventListener('push', event => {
     const payload = event.data.json();
     event.waitUntil(
