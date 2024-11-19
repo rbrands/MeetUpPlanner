e530f4dd8664a5d12b22bb12a5cbf93c3cf03c7b
Robert Brands (RiwaAdmin) <rbadmin@riwa4.de>
2024-11-19 14:19:55 +0100

    Upgrade to .NET 8

--- MeetUpPlanner/Server/Controllers/UtilController.cs
+++ MeetUpPlanner/Server/Controllers/UtilController.cs
@@ -20,7 +20,7 @@ namespace MeetUpPlanner.Server.Controllers
     {
         private readonly MeetUpFunctions _meetUpFunctions;
         private readonly ILogger<UtilController> logger;
-        const string serverVersion = "2024-11-18";
+        const string serverVersion = "2024-11-19";
         string functionsVersion = "tbd";
 
         public UtilController(ILogger<UtilController> logger, MeetUpFunctions meetUpFunctions)
